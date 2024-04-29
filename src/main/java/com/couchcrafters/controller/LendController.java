6ec384626078c897fc5024538eba91aeb020bd37
Hechtei <hechtei0@gmail.com>
2024-04-29 16:19:28 +0200

    Lendings kann man Anlegen Lending hat aber noch keine logik, ob es ein Buch gibt oder nicht.

--- src/main/java/com/couchcrafters/controller/LendController.java
+++ src/main/java/com/couchcrafters/controller/LendController.java
@@ -1,10 +1,39 @@
 package com.couchcrafters.controller;
 
+import com.couchcrafters.model.Book;
+import com.couchcrafters.model.Lending;
+import com.couchcrafters.service.LendService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 public class LendController {
+    @Autowired
+    LendService lendService;
+
+    public LendController(LendService lendService) {
+        this.lendService = lendService;
+    }
+
+    @GetMapping("/createLend")
+    public String createLend(Model model){
+        model.addAttribute("lend", new Lending());
+        return "createLend.html";
+    }
+    @PostMapping("/createLend")
+    public  String createLend(@ModelAttribute("lend") Lending lend){
+        lendService.saveLending(lend);
+        return "createLend.html";
+    }
+
+
 
 }
