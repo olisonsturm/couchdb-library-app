310151a031c72a0a3da93ac9dbcf20725e8840af
Hechtei <hechtei0@gmail.com>
2024-04-29 14:00:46 +0200

    Kundenanllegen
    Kundenanlegen ghet, aber ohne Adresse

--- src/main/java/com/couchcrafters/service/CustomerService.java
+++ src/main/java/com/couchcrafters/service/CustomerService.java
@@ -1,33 +1,28 @@
 package com.couchcrafters.service;
 
 import com.couchcrafters.model.Book;
+import com.couchcrafters.model.Customer;
 import org.lightcouch.CouchDbClient;
 import org.lightcouch.Response;
 
 import java.util.List;
 
-public class BookService {
-    private static CouchDbClient dbClient = new CouchDbClient("couchdb_books.properties");
-    public static void saveBook(Book book) {
-        //Authoren spliten
-        book.setAuthors(book.getAuthors()[0].split(";"));
-        Response response = dbClient.save(book);
+public class CustomerService {
+    private static CouchDbClient dbClient = new CouchDbClient("couchdb_customers.properties");
+    public static void saveCustomer(Customer customer) {
+        Response response = dbClient.save(customer);
         if (response.getError() == null) {
             System.out.println("Dokument wurde erfolgreich hinzugefügt. ID: " + response.getId());
         } else {
             System.err.println("Fehler beim Hinzufügen des Dokuments: " + response.getError());
         }
-
-
     }
 
-    public static List<Book> getAllBooks(){
-        List<Book> books = dbClient.view("_all_docs").includeDocs(true).query(Book.class);
-        for(Book b : books){
+    public static List<Book> getAllCustomers(){
+        List<Book> customers = dbClient.view("_all_docs").includeDocs(true).query(Book.class);
+        for(Book b : customers){
             System.out.println(b.getTitle());
         }
-
-        return books;
+        return customers;
     }
 }
-
